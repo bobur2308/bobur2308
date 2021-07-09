7cc0dee102296d409a38d962996a9069fb275030
Bobur <bobur.niyazov0200@gmail.com>
2021-07-09 22:09:30 +0500

    Some files added

---
+++ ifelse.py
@@ -0,0 +1,64 @@
+import math
+#  1
+a = int(input('Juft son kiriting: '))
+if a % 2 == 1 :
+    print('Rahmat')
+else:
+    print('Bu juft son emas')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ loop.py
@@ -0,0 +1,114 @@
+print('Hello world', 'a')
+
+# while shartlar bloki :
+#     <tanasi>
+
+# i = 1
+# while i <= 10:
+#     print(i)
+#     i+=1 
+# i = 1
+# while i <= 5:
+#     print('salom')
+#     i+=1
+
+# s = 0
+# i = 1 
+# while i <= 10:
+#     s = s + i
+#     i += 1
+# print(s)
+
+# factorila
+# n = int(input('S'))
+# s = 1
+# i = 1
+# while i <= n :
+#     s = s * i
+#     i = i + 1
+# print(s)
+
+# i = 2 
+# g = 2
+# while i <= 9:
+#     while g <= 9:
+#         print(f'{i} * {g} = {i*g}')
+#         g =g+1
+#     g = 2
+#     i = i+1
+# print(round(2.33513,2))
+# i = 1
+# n = int(input('n son = '))
+# x = int(input('x son = '))
+# while i <= n :
+#     s = n / (x **(2*n))
+#     i = i + 1
+# print(s)
+
+
+
+
+
+# for loop
+ 
+# for i in range(9,50):
+#     if i % 2 == 0:
+#         print(i)
+
+# numbers = [1, 4, 7, 8, 15, 20, 35, 45, 55]
+# for i in numbers:
+#     if i > 15:
+#         # break the loop
+#         break
+#     else:
+        # print(i)
+
+# name = "mariya mennen"
+# count = 0
+# for char in name:
+#     if char != 'm':
+#         continue
+#     else:
+#         count = count + 1
+
+# print('Total number of m is:', count)
+
+
+# ism = ['ali ', 'vali', 'bobur', 'husan', 'hasan']
+# for i in ism:
+#         print('salom', i)
+# print(f'Kod {len(ism)} marta takrorlandi')
+
+
+# sonlar = list(range(11,100,2))
+# for son in sonlar:
+#     print(son**3)
+        
+# kinolar = []
+# for i in range(1,6):
+#         s = input(f'{i}chi kinongizni kiriting: ')
+#         kinolar.append(s)
+# print(kinolar)
+
+# kishilar = []
+# s = int(input('Bugun necha odam bilan uchrashdingiz'))
+# for i in range(s):
+#         l = input(f'{i+1}chi uchrashgan odamingiz ismi ')
+#         kishilar.append(l)
+# print(kishilar)
+
+# 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
